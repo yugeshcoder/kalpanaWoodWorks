b5f9fd40c0441cbbdf53cb721919833189d7d9ee
yugeshcoder <yugesh.v.1013@gmail.com>
2024-11-24 13:22:11 +0530

    dependency add

--- ghPage/src/App.jsx
+++ ghPage/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './component/Header'
+import Products from './component/Products'
+import {Routes, Route,Navigate} from 'react-router-dom'
+import AboutUs from './component/AboutUs'
+import ContactUs from './component/ContactUs'
+import TableHeader from './feature/TableHeader'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  const[currentCategory, setCurrentCategory] = useState("plain");
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header setCurrentCategory = {setCurrentCategory}/>
+      <Routes>
+        <Route path='/products' element ={<Products currentCategory ={ currentCategory} setCurrentCategory = {setCurrentCategory}/>}></Route>
+        <Route path='/' element={<Products currentCategory ={ currentCategory} setCurrentCategory = {setCurrentCategory}/>}></Route>
+        <Route path='*' element={<Products currentCategory ={ currentCategory} setCurrentCategory = {setCurrentCategory}/>}></Route>
+        <Route path = '/about' element = {<AboutUs/>}></Route>
+        <Route path='/contact' element = {<ContactUs/>}></Route>
+        <Route path='/calc' element={<TableHeader/>}></Route>
+      </Routes>
     </>
   )
 }
